c7637d0cba582d2b8d096c1d8e607883f6691956
venom <glizbeth@azc.uam.mx>
2025-04-15 21:03:26 -0600

    Recupera los datos de un csv publicado en google sheet

--- src/api.ts
+++ src/api.ts
@@ -121,9 +121,24 @@ const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, Math.
 const api = {
   // Obtener todos los restaurantes
   list: async (): Promise<Restaurant[]> => {
-    // Simular un delay en la respuesta de la API
-    await sleep(750);
+    // Obtenemos la información de Google Sheets en formato texto y la dividimos por líneas, nos saltamos la primera línea porque es el encabezado
+    const [, ...data] = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTASP57CHhGPqg9ULdqRWlCLrnspiy4bPiYQKVXMIsGcRFcCu0UBjgbVFciBWy82iTBF5TFFL3QxBRw/pub?output=csv').then(res => res.text()).then(text => text.split('\n'))
 
+    // Convertimos cada línea en un objeto Restaurant, asegúrate de que los campos no posean `,`
+    const restaurants: Restaurant[] = data.map((row) => {
+      const [id, name, description, address, score, ratings, image] = row.split(',')
+      return {
+        id,
+        name,
+        description,
+        address,
+        score: Number(score),
+        ratings: Number(ratings),
+        image
+      }
+    })
+
+    // Lo retornamos
     return restaurants;
   },
   // Obtener un restaurante específico por su ID
--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,12 +1,22 @@
 import api from "@/api";
 import RestaurantCard from "../components/restaurantCard";
 
+export async function generateMetadata({params}: {params: Promise<{id: string}>}) {
+    const {id} = await params;
+    const restaurant = await api.fetch(id);
+  
+    return {
+      title: `${restaurant.name} - Restaurancy`,
+      description: restaurant.description,
+    };
+  }
+
 export default async function RestaurantPage({params}: {params: Promise<{id: string}>}) {
   const {id} = await params;
   const restaurant = await api.fetch(id);
 
   return (
     <RestaurantCard restaurant={restaurant} />
-    
+
   );
 }
